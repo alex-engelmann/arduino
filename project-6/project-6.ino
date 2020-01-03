36a4f9f44efa68489f146d9e900ed7d92e618b40
Alex Engelmann <alex.engelmann@gmail.com>
2020-01-02 22:51:33 -0500

    project 6, theremin

--- project-6/project-6.ino
+++ project-6/project-6.ino
@@ -2,14 +2,37 @@
 
 int sensorValue;
 int sensorLow = 1023;
-int sensoHigh = 0;
+int sensorHigh = 0;
+
+const int ledPin = 13;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 
+  while (millis() < 5000) {
+
+    sensorValue = analogRead(A0);
+    //calibrate a range of sensor values for baseline room light conditions
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow) {
+      sensorHigh = sensorValue;
+    }
+
+  }
+  //indicates end of calibration by turning LED off
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(A0);
+  //B0 to B6
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 31, 1976);
+  //tone function parameters (pin, Hz, ms)
+  tone(8, pitch, 20);
+
+  delay(10);
 
 }
